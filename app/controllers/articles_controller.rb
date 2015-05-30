e1d7ad946c1e2fb4187f6a4fb34d8c9d0e12ca7a
JJLim <lim.jess9@gmail.com>
2015-05-30 13:46:56 -0400

    Add edit and update actions for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,12 +12,26 @@ class ArticlesController < ApplicationController
     @article = Article.new # so @article will not be nil in the view when checking for errors
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save #returns a boolean
       redirect_to @article
     else
       render 'new' #use render instead of redirect_to so the @articble object passed to new template
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
